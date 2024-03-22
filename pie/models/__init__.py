85fcc90400721e7d03fa7a3a33dabee2001e4126
Thibault Clérice <leponteineptique@gmail.com>
2024-03-22 11:13:05 +0100

    Remove pretrained embeddings import from models

--- pie/models/__init__.py
+++ pie/models/__init__.py
@@ -5,5 +5,4 @@ from .encoder import RNNEncoder
 from .embedding import CNNEmbedding, RNNEmbedding, EmbeddingConcat, EmbeddingMixer
 from .embedding import build_embeddings
 from .decoder import LinearDecoder, AttentionalDecoder, CRFDecoder
-from .loaders import get_pretrained_embeddings
 from .scorer import Scorer, compute_scores
